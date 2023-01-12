95ebcc72436a7b53a06ccb57c1e5db8a6227d941
ViolaRafael <rafaelviola.eprosec@gmail.com>
2023-01-12 11:56:11 -0100

    Add Auth Page Logic

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,5 +1,6 @@
 import 'package:chat/components/auth_form.dart';
-import 'package:chat/models/auth_form_data.dart';
+import 'package:chat/core/models/auth_form_data.dart';
+import 'package:chat/core/services/auth/auth_mock_service.dart';
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatefulWidget {
@@ -12,12 +13,30 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   bool _isLoading = false;
 
-  void _handleSubmit(AuthFormData formData) {
-    setState(() => _isLoading = true);
-    print('AuthPage...');
-    print(formData.email);
+  Future<void> _handleSubmit(AuthFormData formData) async {
+    try {
+      setState(() => _isLoading = true);
 
-    setState(() => _isLoading = false);
+      if (formData.isLogin) {
+        //Login
+        await AuthMockService().login(
+          formData.email,
+          formData.password,
+        );
+      } else {
+        //Signup
+        await AuthMockService().signup(
+          formData.name,
+          formData.email,
+          formData.password,
+          formData.image,
+        );
+      }
+    } catch (error) {
+      //Tratar erro
+    } finally {
+      setState(() => _isLoading = false);
+    }
   }
 
   @override
@@ -33,7 +52,8 @@ class _AuthPageState extends State<AuthPage> {
           ),
           if (_isLoading)
             Container(
-              decoration: const BoxDecoration(color: Color.fromRGBO(0, 0, 0, 0.5)),
+              decoration:
+                  const BoxDecoration(color: Color.fromRGBO(0, 0, 0, 0.5)),
               child: const Center(
                 child: LinearProgressIndicator(),
               ),
