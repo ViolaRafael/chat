8507aec48346d229d671f022c1f00c28382e5239
ViolaRafael <rafaelviola.eprosec@gmail.com>
2023-01-12 16:31:53 -0100

    Refactor code to use factory construct

--- lib/core/services/auth/auth_service.dart
+++ lib/core/services/auth/auth_service.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:chat/core/models/chat_user.dart';
+import 'package:chat/core/services/auth/auth_mock_service.dart';
 
 abstract class AuthService {
   ChatUser? get currentUser;
@@ -17,4 +18,8 @@ abstract class AuthService {
     String password,
   );
   Future<void> logout();
+
+  factory AuthService() {
+    return AuthMockService();
+  }
 }
